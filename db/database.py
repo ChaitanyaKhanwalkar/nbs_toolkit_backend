a0a73e4e92355e08708bea6831f1ee51c5ee6b29
ChaitanyaKhanwalkar <chaitanya.khanwalkar@gmail.com>
2025-07-10 19:28:43 +0530

    Update database.py

--- db/database.py
+++ db/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+print("ENV VARS:", os.environ)
+
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("postgresql://nbs_database_user:olbzqH91si07EtEF7xXhYHxeHxOJM4s2@dpg-d1migujipnbc739lkivg-a/nbs_database")
 
