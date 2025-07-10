55f3b353ad8c05196db8188b30d294bb69467916
ChaitanyaKhanwalkar <chaitanya.khanwalkar@gmail.com>
2025-07-10 14:02:47 +0530

    Update database.py

--- db/database.py
+++ db/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+print("DATABASE_URL:", SQLALCHEMY_DATABASE_URL)
+
 SQLALCHEMY_DATABASE_URL = os.environ.get("postgresql://nbs_database_user:olbzqH91si07EtEF7xXhYHxeHxOJM4s2@dpg-d1migujipnbc739lkivg-a/nbs_database")
 
 engine = create_engine(DATABASE_URL)
