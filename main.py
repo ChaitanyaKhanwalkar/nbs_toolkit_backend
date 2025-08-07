f5312c4bbfa7ffd45c9bba8f89cbd3d19ea56022
ChaitanyaKhanwalkar <chaitanya.khanwalkar@gmail.com>
2025-08-07 15:54:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from db import models
 from api import location
 from api import recommendations
 from api import implementation
+import threading
+import time
+import requests
+
 
 
 app = FastAPI()
@@ -24,3 +28,14 @@ app.include_router(implementation.router)
 @app.get('/')
 def read_root():
     return {"message": "Welcome to the NbS Toolkit API"}
+
+def keep_alive():
+    while True:
+        try:
+            requests.get("https://nbs-toolkit-backend.onrender.com/recommendations?state_name=Madhya%20Pradesh&water_type=Grey%20Water")
+        except Exception as e:
+            print(f"Keep-alive failed: {e}")
+        time.sleep(300)  # Ping every 10 min
+
+threading.Thread(target=keep_alive, daemon=True).start()
+
